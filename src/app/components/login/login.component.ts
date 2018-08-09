6505a8964fe08f88beddccbf8e4a03277e3c7d7f
Eduardo <eduardocastrogt@gmail.com>
2018-08-08 18:44:45 -0600

    subiendo

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User';
+import { UserService } from '../../services/user.service';
+import { Response } from '../../../../node_modules/@angular/http';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  providers: [
+    UserService
+  ]
+})
+export class LoginComponent implements OnInit {
+
+  public user: User;
+
+
+
+  constructor(
+    private _userService: UserService
+  ) {
+     
+    this.user = new User('','','','','','');
+   }
+
+  ngOnInit() {
+  }
+
+  onSubmit(){
+    this._userService.login(this.user)
+      .subscribe(
+        response => {
+          console.log(response);
+        },
+        error =>{
+          console.log(error)
+        }
+      );
+  }
+
+}
